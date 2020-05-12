1070302c21cf7a8259222f41bea027ab355b2c1f
Krithic Kumar <30691152+jedi2610@users.noreply.github.com>
2020-05-12 23:10:19 +0530

    Added support for resizing all images present in a directory

--- resizer.py
+++ resizer.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+from glob import glob
+from pathlib import Path
 from fractions import Fraction
 from PIL import Image
 
@@ -31,8 +33,7 @@ class Resize():
             print_properties('image (if resized)', dimensions)
             print("\nThe given dimensions do not match the aspect ratio of the original image.")
             option = input("Do you want to force resize it?(Y/N): ").lower()
-            if option == 'y':
-                return 'y'
+            return option
 
 
 def print_properties(text, size):
@@ -50,74 +51,113 @@ def main(args):
     """
     Driver Code
     """
-    imgPath = args.input
-    outPath = args.output
-    outPath = args.output
+    imgExtensions = ['.png', '.jpg', '.jpeg', '.ppm', '.tiff', '.bmp']
+    if args.force == True:
+            args.aspect = False
     size = tuple(args.size)
-
-    # Check if the given image path exists
-    if not os.path.isfile(imgPath):
-        print("\nCannot find the given file", imgPath)
-        print("Make sure the file exists and the path contains the image extension.")
-        print_exit()
-        exit(1)
-
+    imageFilePaths = list()
+    
     # Check if the given dimensions are valid
     if len(size) != 2:
         print("\nThe given size", size, "is not supported.")
         print_exit()
         exit(1)
-
-    imgFile = Image.open(imgPath)
-    fileFormat = imgFile.format
-    imgObject = Resize(imgFile)
-    outFile = None
-    originalSize = imgFile.size
-
-    #  Printing properties of the original image
-    print_properties('original image', originalSize)
     
-    # Checking if the '-f' tag is tagged
-    if args.force:
-        outFile = imgObject.forceResize(size)
+    # Checking if the given argument is a file or a folder
+    if os.path.isfile(args.input):
+        if Path(args.input).suffix in imgExtensions:
+            imageFilePaths.append(args.input)
+        else:
+            print("Error. The given file is not an image.")
 
-    # Checking if the '-a' tag is tagged
-    elif args.aspect:
-        outFile = imgObject.aspectResize(size)
-        if outFile == 'y':
-            outFile = imgObject.forceResize(size)
+    elif os.path.isdir(args.input):
+        # Gets the path of all the image files in a given directory (sub-directories included)
+        for extensions in imgExtensions:
+            imageFilePaths += glob('{}\\**/*{}'.format(args.input, extensions), recursive=True)
 
-    # Checking if the outFile is healthy
-    if outFile == None:
-        print("\nAn error occured. Try again with another image.")
+    else:
+        print("\nCannot find the given file/directory", args.input)
+        print("Make sure the file/directory exists and the path contains the image extension if it's an image file.")
         print_exit()
         exit(1)
 
-    # Printing properties of the resized image
-    newSize = outFile.size
-    print_properties('resized image', newSize)
+    # Check if the list contains atleast 1 image file path
+    if len(imageFilePaths) == 0:
+        print("The directory has no image files.")
+        print_exit()
+        exit(0)
+        
+    try:
+        # Resize all the images in the list
+        for filePath in imageFilePaths:
 
-    # Saving the resized image
-    outFile.show()
-    if outPath:
-        try:
-            outFile.save(outPath, fileFormat)
-            print("\nThe image is resized and saved at: \"" + outPath + "\"")
-        except IOError:
-            print(
-                "\nAn error occured while saving the resized image. Check if the given path exists.")
-            print_exit()
-            exit(1)
-    else:
-        outPath = '\\'.join(imgPath.split('\\')[0:-1])
-        outPath += "\\" + ''.join(imgPath.split('\\')[-1].split('.')[0:-1]) + "_resized." + fileFormat
-        try:
-            outFile.save(outPath, fileFormat)
-            print("\nThe image is resized and saved at: \"" + outPath + "\"")
-        except IOError:
-            print("\nAn error occured while saving the resized image")
-            print_exit()
-            exit(1)
+            imageName = os.path.basename(filePath)
+            outPath = os.path.dirname(filePath) + "\\Resized_Images\\\\"
+            try:
+                os.makedirs(outPath, exist_ok=True)
+            except OSError:
+                print("An error occured while creating the directory.")
+                print_exit()
+                exit(1)
+
+            imgFile = Image.open(filePath)
+            fileFormat = imgFile.format
+            imgObject = Resize(imgFile)
+            originalSize = imgFile.size
+            outFile = None
+
+            #  Printing properties of the original image
+            print_properties(f'original image "{imageName}"', originalSize)
+            
+            # Checking if the '-f' tag is tagged
+            if args.force:
+                outFile = imgObject.forceResize(size)
+
+            # Checking if the '-a' tag is tagged
+            elif args.aspect:
+                outFile = imgObject.aspectResize(size)
+                if outFile == 'y':
+                    outFile = imgObject.forceResize(size)
+                elif outFile == 'n':
+                    print('------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+                    continue
+
+            # Checking if the outFile is healthy
+            if outFile == None:
+                print("\nAn error occured. Try again with another image.")
+                print_exit()
+                exit(1)
+
+            # Printing properties of the resized image
+            newSize = outFile.size
+            print_properties(f'resized image "{imageName}"', newSize)
+
+            # outFile.show()
+            # Saving the resized image
+            if args.output:
+                # TODO implement this method if the given path is a folder
+                try:
+                    outFile.save(args.output, fileFormat)
+                    print("\nThe image is resized and saved at: \"" + args.output + "\"")
+                except IOError:
+                    print("\nAn error occured while saving the resized image. Check if the given path exists.")
+                    print_exit()
+                    continue
+            else:
+                outPath += imageName.split('.')[0]
+                outPath += "_resized." + fileFormat
+                try:
+                    outFile.save(outPath, fileFormat)
+                    print("\nThe image is resized and saved at: \"" + outPath + "\"")
+                except IOError:
+                    print("\nAn error occured while saving the resized image")
+                    print_exit()
+                    continue
+
+                print('------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+    except KeyboardInterrupt:
+        print("\nProcess terminated.")
+        exit(0)
 
 
 if __name__ == "__main__":
@@ -125,7 +165,7 @@ if __name__ == "__main__":
     # Parsing command line args
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help='Path of the input file.')
-    parser.add_argument('size', nargs='+', type=int,  help='The desired dimensions in pixels as a tuple: (w, h).')
+    parser.add_argument('size', nargs='+', type=int, help='The desired dimensions in pixels as a tuple: (w, h).')
     parser.add_argument('-a', '--aspect', action='store_true', default=True, dest='aspect', help='Forces to stick with the aspect ratio of the original image.[Default = True]')
     parser.add_argument('-f', '--force', action='store_true', dest='force', help='Force resize the image even if the given dimensions are not on par with the original image dimensions.')
     parser.add_argument('-o', '--output', dest='output', type=str, help='Path of the output file without extension.')
